48f984ea47bbb7fa42b1e6779cb02f56359869a0
Alan Reis <140574199+AlanBReis@users.noreply.github.com>
2024-01-22 20:19:20 -0300

    adicionado function nas teclas

---
+++ main.js
@@ -0,0 +1,53 @@
+function audioPom() {
+    document.querySelector("#som_tecla_pom").play();
+}
+
+document.querySelector(".tecla_pom").onclick = audioPom;
+
+function audioClap() {
+    document.querySelector("#som_tecla_clap").play();
+}
+
+document.querySelector(".tecla_clap").onclick = audioClap;
+
+function audioTim() {
+    document.querySelector("#som_tecla_tim").play();
+}
+
+document.querySelector(".tecla_tim").onclick = audioTim;
+
+function audioPuff() {
+    document.querySelector("#som_tecla_puff").play();
+}
+
+document.querySelector(".tecla_puff").onclick = audioPuff;
+
+function audioSplash() {
+    document.querySelector("#som_tecla_splash").play();
+}
+
+document.querySelector(".tecla_splash").onclick = audioSplash;
+
+function audioToim() {
+    document.querySelector("#som_tecla_toim").play();
+}
+
+document.querySelector(".tecla_toim").onclick = audioToim;
+
+function audioPsh() {
+    document.querySelector("#som_tecla_psh").play();
+}
+
+document.querySelector(".tecla_psh").onclick = audioPsh;
+
+function audioTic() {
+    document.querySelector("#som_tecla_tic").play();
+}
+
+document.querySelector(".tecla_tic").onclick = audioTic;
+
+function audioTom() {
+    document.querySelector("#som_tecla_tom").play();
+}
+
+document.querySelector(".tecla_tom").onclick = audioTom;
