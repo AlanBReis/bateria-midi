0833afc0a223df3d2ecc10ff30c7481025fcb391
Alan Reis <140574199+AlanBReis@users.noreply.github.com>
2024-01-22 21:22:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,15 @@
-function audioPom() {
-    document.querySelector("#som_tecla_pom").play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector(".tecla_pom").onclick = audioPom;
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-function audioClap() {
-    document.querySelector("#som_tecla_clap").play();
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSom;
+
+    contador = contador + 1;
+
+    console.log(contador);
 }
-
-document.querySelector(".tecla_clap").onclick = audioClap;
-
-function audioTim() {
-    document.querySelector("#som_tecla_tim").play();
-}
-
-document.querySelector(".tecla_tim").onclick = audioTim;
-
-function audioPuff() {
-    document.querySelector("#som_tecla_puff").play();
-}
-
-document.querySelector(".tecla_puff").onclick = audioPuff;
-
-function audioSplash() {
-    document.querySelector("#som_tecla_splash").play();
-}
-
-document.querySelector(".tecla_splash").onclick = audioSplash;
-
-function audioToim() {
-    document.querySelector("#som_tecla_toim").play();
-}
-
-document.querySelector(".tecla_toim").onclick = audioToim;
-
-function audioPsh() {
-    document.querySelector("#som_tecla_psh").play();
-}
-
-document.querySelector(".tecla_psh").onclick = audioPsh;
-
-function audioTic() {
-    document.querySelector("#som_tecla_tic").play();
-}
-
-document.querySelector(".tecla_tic").onclick = audioTic;
-
-function audioTom() {
-    document.querySelector("#som_tecla_tom").play();
-}
-
-document.querySelector(".tecla_tom").onclick = audioTom;
